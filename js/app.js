90df6b4b5ff47705b32c8c9f9fdac610e50389e8
Vickingoo <vazquezgarridovictor@gmail.com>
2022-05-02 16:03:11 +0200

    juego de piedra papel tijeras

---
+++ js/app.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", () => {
+    // querying elements
+    let playerScore = document.querySelector(".row.bottom").firstElementChild;
+    let cpuScore = document.querySelector(".row.bottom").lastElementChild;
+    let roundNumber = document.querySelector(".round");
+    let display = document.querySelector(".display");
+    let userMove =
+      document.querySelector(".results").firstElementChild.firstElementChild;
+    let cpuMove =
+      document.querySelector(".results").lastElementChild.firstElementChild;
+  
+    let option1 =
+      document.querySelector(".play").firstElementChild.nextElementSibling;
+    let option2 = option1.nextElementSibling;
+    let option3 = option2.nextElementSibling;
+  //   console.log(option3);
+  });
